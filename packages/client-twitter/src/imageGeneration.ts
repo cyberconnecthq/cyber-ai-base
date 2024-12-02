5eda4b74389fbfc2796eefe0047b362260a4b444
ZimHook <zim764723426@gmail.com>
2024-12-02 10:42:28 +0800

    openai prompt

--- packages/client-twitter/src/imageGeneration.ts
+++ packages/client-twitter/src/imageGeneration.ts
@@ -3,10 +3,38 @@ import { elizaLogger } from "@ai16z/eliza";
 export const ChibsModelId = "9d758849-365e-4cf5-8dc1-0fdd9bc3209c";
 
 export const promptForChibs = (prompt: string) => {
-    let p = prompt.toLowerCase().replaceAll("chibs", "penguin").replaceAll("chibling", "penguin");
-    p += "; cartoon style; cute white eyes with black eyeball; flat color background;";
-    p = "draw a penguin cartoon for this tweet:"
+    let p = prompt
+        .toLowerCase()
+        .replaceAll("chibs", "penguin")
+        .replaceAll("chibling", "penguin");
+    p +=
+        "; cartoon style; cute white eyes with black eyeball; flat color background;";
+    p = "draw a penguin cartoon for this tweet:";
     return p;
+};
+
+export const promptForChibsByGpt = async (prompt: string) => {
+    const response = await (
+        await fetch("https://api.openai.com/v1/chat/completions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+            body: JSON.stringify({
+                model: "gpt-4o-mini",
+                messages: [
+                    {
+                        role: "user",
+                        content:
+                            "You are a prompt engineer, parse this text and generate prompt for an image generation which go with this text: " +
+                            prompt,
+                    },
+                ],
+            }),
+        })
+    ).json();
+    return response?.choices?.[0]?.message?.content;
 };
 
 export const generateImage = async (prompt: string, modelId: string) => {
